714fe6b29fab7701e6a6aa27ac44561421a96d0c
naxifa <maliha1@usf.edu>
2024-09-08 01:01:57 -0400

    Create and test calculateTip function

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,19 +1,57 @@
 
 // Task 1: Calculate the tip 
 
-let bill=x; // Example of the bill
-let x=60;
+let bill = 60; // Example of the bill
 let tip;
 
-if(x>= 50 && x<=300) {
-    tip = x * .15;
+if(bill>= 50 && bill<=300) {
+    tip = (bill) * .15;
 }
-else{tip = x * .20;
+else{tip = (bill) * .20;
 }
 
 
 // Task 2: Output Details
 
-const total = bill + tip;
+let total = bill + tip;
 
 console.log(`The bill was $${bill}, the tip was $${tip}, and the total value is $${total}`);
+
+
+// Task 3: Create a function
+
+function calculateTip(bill) {  
+    let tip;
+    if(bill>= 50 && bill<=300) {
+        tip = (bill) * .15;
+    }
+    else{tip = (bill) * .20;
+    }
+    return tip }; 
+
+    bill = 100;
+    tip = calculateTip(bill);
+    total = bill + tip
+
+    console.log(`The bill was $${bill}, the tip was $${tip}, and the total value is $${total}`);
+
+
+    // Task 4: Utilize Arrays 
+    // Data Set 1 
+
+    let bills = [275,40,430];
+    let tips = bills.map(bill => calculateTip(bill));
+
+    console.log("Bills: ", bills);
+    console.log("Tips: ", tips);
+
+    let totals = bills.map((bills, index) => bills + tips[index]);
+
+    console.log("Totals: ", totals);
+    
+    bills.forEach((bill, index) => 
+         { let tip = tips[index];
+            let total = bill + tip;
+            console.log(`The bill was $${bill}, the tip was $${tip}, and the total value is $${total}`)});
+
+           
